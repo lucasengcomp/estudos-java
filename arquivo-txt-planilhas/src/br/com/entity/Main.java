18b38ae8473cc4787c8fb9d933c5b801d8316947
Lucas Galvao <lucas.engcomp@outlook.com>
2022-07-17 03:40:45 -0300

    Escrita simples em um txt

--- arquivo-txt-planilhas/src/br/com/entity/Main.java
+++
@@ -1,4 +0,0 @@
-package br.com.entity;
-
-public class Main {
-}
--- arquivo-txt-planilhas/src/br/com/program/Main.java
+++ arquivo-txt-planilhas/src/br/com/program/Main.java
@@ -1,4 +1,23 @@
 package br.com.program;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Main {
+
+    public static void main(String[] args) throws IOException {
+        File arquivo = new File("C:\\projetos\\estudos\\arquivo-txt-planilhas\\src\\br\\com\\utils\\arquivo.txt");
+
+        if (!arquivo.exists()) {
+            arquivo.createNewFile();
+        }
+
+        FileWriter escreverNoArquivo = new FileWriter(arquivo);
+
+        escreverNoArquivo.write("Meu texto no arquivo\n");
+        escreverNoArquivo.write("Outro texto de teste escrita");
+        escreverNoArquivo.flush();
+        escreverNoArquivo.close();
+    }
 }
