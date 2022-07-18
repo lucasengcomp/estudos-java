d1af67ff13e5ceae39ca0a1502026be8a4f41518
Lucas Galvao <lucas.engcomp@outlook.com>
2022-07-18 08:31:52 -0300

    Estrutura refatorada para trabalhar apenas com arquivos

---
+++ arquivo-txt/src/br/com/util/Consts.java
@@ -0,0 +1,7 @@
+package br.com.util;
+
+public class Consts {
+
+    public static final String CAMINHO_ARQUIVO_PESSOA = "C:\\projetos\\estudos\\arquivo-txt\\src\\br\\com\\geraarquivo\\arquivotxt\\arquivoPessoa.txt";
+    public static final String CAMINHO_ARQUIVO = "C:\\projetos\\estudos\\arquivo-txt\\src\\br\\com\\geraarquivo\\arquivotxt\\arquivo.txt";
+}
