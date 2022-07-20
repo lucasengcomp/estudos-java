d72d43ca95d3ea226c3f390f1639ac9564b78fd5
Lucas Galvao <lucas.engcomp@outlook.com>
2022-07-20 00:06:59 -0300

    Escrita de dados na planilha usando Object

--- arquivo-planilha/src/main/java/operacoes/Escrita.java
+++ arquivo-planilha/src/main/java/operacoes/Escrita.java
@@ -15,33 +15,53 @@ public class Escrita {
         XSSFSheet sheet = workbook.createSheet("Aba Planilha Trabalhadores");
 
         Object dados[][] = {{"ID", "Nome", "Profissao"},
-                {111, "Lucas", "Agricultor"},
-                {222, "Ana", "Engenheira"},
-                {333, "Ivone", "Violeira"},
-                {444, "Bruna", "Sanfoneira"}
+                {351, "Lucas", "Agricultor"},
+                {521, "Ana", "Engenheira"},
+                {572, "Ivone", "Violeira"},
+                {701, "Bruna", "Sanfoneira"}
         };
 
-        int linha = dados.length;
-        int coluna = dados[0].length;
+//        int linha = dados.length;
+//        int coluna = dados[0].length;
+//
+//        for (int i = 0; i < linha; i++) {
+//            XSSFRow linhaCriada = sheet.createRow(i);
+//
+//            for (int j = 0; j < coluna; j++) {
+//                XSSFCell celulaCriada = linhaCriada.createCell(j);
+//                Object value = dados[i][j];
+//
+//                if (value instanceof String)
+//                    celulaCriada.setCellValue((String) value);
+//                if (value instanceof Integer)
+//                    celulaCriada.setCellValue((Integer) value);
+//                if (value instanceof Boolean)
+//                    celulaCriada.setCellValue((Boolean) value);
+//            }
+//            FileOutputStream outstream = new FileOutputStream("src/main/java/planilhas/trabalhadores.xlsx");
+//            workbook.write(outstream);
+//            outstream.close();
+//        }
+//        System.out.println("Planilha criada com sucesso!");
+        int contadorLinha = 0;
 
-        for (int i = 0; i < linha; i++) {
-            XSSFRow linhaCriada = sheet.createRow(i);
+        for (Object registro[] : dados) {
+            XSSFRow linhaCriada = sheet.createRow(contadorLinha++);
+            int contadorColuna = 0;
+            for (Object valorEscrito : registro) {
+                XSSFCell celulaCriada = linhaCriada.createCell(contadorColuna++);
 
-            for (int j = 0; j < coluna; j++) {
-                XSSFCell celulaCriada = linhaCriada.createCell(j);
-                Object value = dados[i][j];
-
-                if (value instanceof String)
-                    celulaCriada.setCellValue((String) value);
-                if (value instanceof Integer)
-                    celulaCriada.setCellValue((Integer) value);
-                if (value instanceof Boolean)
-                    celulaCriada.setCellValue((Boolean) value);
+                if (valorEscrito instanceof String)
+                    celulaCriada.setCellValue((String) valorEscrito);
+                if (valorEscrito instanceof Integer)
+                    celulaCriada.setCellValue((Integer) valorEscrito);
+                if (valorEscrito instanceof Boolean)
+                    celulaCriada.setCellValue((Boolean) valorEscrito);
             }
-            FileOutputStream outstream = new FileOutputStream("src/main/java/planilhas/trabalhadores.xlsx");
-            workbook.write(outstream);
-            outstream.close();
         }
+        FileOutputStream outstream = new FileOutputStream("src/main/java/planilhas/trabalhadores.xlsx");
+        workbook.write(outstream);
+        outstream.close();
         System.out.println("Planilha criada com sucesso!");
     }
 }
