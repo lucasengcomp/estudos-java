353d53e934faac8b94b5a505b5538b090bb7ea74
Lucas Galvao Lima <lucas.engcomp@outlook.com>
2022-07-22 02:32:22 -0300

    Projeto criado

---
+++ estrutura-de-dados/src/com/br/estrutura/vetor/Vetor.java
@@ -0,0 +1,12 @@
+package com.br.estrutura.vetor;
+
+public class Vetor {
+
+	private String[] elementos;
+	
+	public Vetor(int capacidade) {
+		this.elementos = new String[capacidade];
+	}
+	
+	
+}
---
+++ estrutura-de-dados/src/com/br/estrutura/vetor/teste/VetorTest.java
@@ -0,0 +1,14 @@
+package com.br.estrutura.vetor.teste;
+
+import com.br.estrutura.vetor.Vetor;
+
+public class VetorTest {
+
+	public static void main(String[] args) {
+		Vetor vetor = new Vetor(5);
+
+		int i = 0;
+		i++;
+	}
+
+}
